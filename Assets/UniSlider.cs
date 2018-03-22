2875e351fddd84e6995dde54ed8e7e8e05f60fb7
Michael Gackle <talscar@hotmail.com.au>
2018-03-22 16:12:23 +1030

    #Joystick complete

--- Assets/UniSlider.cs
+++ Assets/UniSlider.cs
@@ -11,12 +11,15 @@ public class UniSlider : MonoBehaviour,  IPointerUpHandler, IPointerDownHandler 
     public float limit;
     public Transform handle;
 
+    //public Vector2 minMaxValues = new Vector2(-1, 1);
+    public float minMaxValue = 1f;
+    public Vector2 value;
     //float rate = transform.position - handle.position;
 
 	// Use this for initialization
 	void Start () {
         //newPointer = GetComponent<PointerEventData>();
-
+        valuePerUnit = minMaxValue / limit;
     }
 
     bool carryHandle = false;
@@ -25,23 +28,49 @@ public class UniSlider : MonoBehaviour,  IPointerUpHandler, IPointerDownHandler 
     {
         newPointer = eventdata;
         carryHandle = true;
-        Debug.Log("eventdata: " + eventdata);
+        //Debug.Log("eventdata: " + eventdata);
     }
     //public void OnPointer
     public void OnPointerUp(PointerEventData eventdata)
     {
         carryHandle = false;
-        Debug.Log("eventdata: " + eventdata);
+        //Debug.Log("eventdata: " + eventdata);
         handle.transform.position = transform.position;
     }
+    public float valuePerUnit;
 
     PointerEventData newPointer;
+
+    //public Vector2 c
     // Update is called once per frame
     void Update () {
 		if(carryHandle)
         {
-            handle.position = new Vector3(newPointer.position.x, newPointer.position.y, 0);
+            Vector3 centerPosition = transform.position; //center of *black circle*
+            Vector3 newPos = new Vector3(newPointer.position.x, newPointer.position.y, 0);
+            float distance = Vector3.Distance(newPos, centerPosition);
+            if (distance > limit) //If the distance is less than the radius, it is already within the circle.
+            {
+                Vector3 fromOriginToObject = newPos - centerPosition; //~GreenPosition~ - *BlackCenter*
+                fromOriginToObject *= limit / distance; //Multiply by radius //Divide by Distance
+                newPos = centerPosition + fromOriginToObject; //*BlackCenter* + all that Math
+                distance = Vector3.Distance(newPos, centerPosition);
+                //minMaxValues / distance;
+
+            }
+
+            handle.position = newPos; 
         }
+            value = new Vector2((handle.localPosition.x - position.x) * valuePerUnit, (handle.localPosition.y - position.y) * valuePerUnit).normalized * valuePerUnit;
 
 	}
 }
+// Vector3 centerPosition = transform.localPosition; //center of *black circle*
+//float distance = Vector3.Distance(newLocation, centerPosition);
+
+// if (distance > radius) //If the distance is less than the radius, it is already within the circle.
+// {
+// Vector3 fromOriginToObject = newLocation - centerPosition; //~GreenPosition~ - *BlackCenter*
+//fromOriginToObject *= radius / distance; //Multiply by radius //Divide by Distance
+// newLocation = centerPosition + fromOriginToObject; //*BlackCenter* + all that Math
+// }
